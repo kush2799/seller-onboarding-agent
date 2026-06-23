bc3225cd5c35c38a53051fe0e2fa9c111f43a37f
kush2799 <kushagrabachhil@gmail.com>
2026-06-23 10:46:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ st.set_page_config(
 st.title("📄 Seller Onboarding Agent")
 
 seller_name = st.text_input("Seller Name")
+email = st.text_input("Email")
+phone = st.text_input("Phone Number")
 
 uploaded_files = st.file_uploader(
     "Upload Documents",
@@ -15,10 +17,16 @@ uploaded_files = st.file_uploader(
 )
 
 if st.button("Submit"):
-    st.success("Documents uploaded successfully!")
 
-if uploaded_files:
-    st.subheader("Uploaded Files")
+    st.success("Application Submitted")
 
-    for file in uploaded_files:
-        st.write(f"✅ {file.name}")
+    st.write("### Summary")
+    st.write("Seller:", seller_name)
+    st.write("Email:", email)
+    st.write("Phone:", phone)
+
+    if uploaded_files:
+        st.write("### Uploaded Documents")
+
+        for file in uploaded_files:
+            st.write(f"✅ {file.name}")
